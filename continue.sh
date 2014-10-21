bb21e0497e48474f80f31ce0244971e8fbc3c2d5
David Renshaw <dwrenshaw@gmail.com>
2014-10-21 15:36:40 -0400

    use nginx. pulling and pushing both work

--- continue.sh
+++ continue.sh
@@ -1,7 +1,12 @@
 #! /bin/sh
 
+echo "starting fcgiwrap..."
+fcgiwrap -f -s tcp:127.0.0.1:9000 &
+echo "started fcgiwrap: " $?
+
 echo "starting http server..."
-#httpd -f /etc/httpd/conf/httpd.conf
-apachectl start
+#httpd -f /etc/httpd/conf/httpd.conf -e debug
+#apachectl start
+/usr/bin/nginx
 echo "started http server."
 sleep infinity
--- start.sh
+++ start.sh
@@ -1,10 +1,19 @@
 #! /bin/sh
 
 mkdir -p /var/git
+mkdir -p /var/lib/nginx
+mkdir -p /var/log/nginx
 mkdir -p /var/log/httpd
-mkdir -p /var/run
+mkdir -p /var/run/httpd
+
 
 cd /var/git
-git init --bare .
+git --bare init --shared
+git config --file config http.receivepack true
+git update-server-info
 
-sh /continue.sh
+cat config
+
+cd /
+
+sh continue.sh
