fbac5d8880d7d879cad3310dc016792ea38b6727
David Renshaw <dwrenshaw@gmail.com>
2014-12-16 09:56:37 -0500

    git -> repo.git

--- start.sh
+++ start.sh
@@ -6,9 +6,9 @@ mkdir -p /var/lib/nginx
 mkdir -p /var/log/nginx
 mkdir -p /var/run
 
-mkdir -p /var/git
+mkdir -p /var/repo.git
 
-cd /var/git
+cd /var/repo.git
 git --bare init --shared
 git config --file config http.receivepack true
 git update-server-info
