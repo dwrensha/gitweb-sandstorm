7791303f42e3f673f4ad1892402d694f8d5b671e
David Renshaw <david@sandstorm.io>
2015-07-31 15:12:12 -0400

    Spaces in front of copy/pastable commands, making it less likely that bash will remember your password.

--- srv/http/gitweb/sandstorm.js
+++ srv/http/gitweb/sandstorm.js
@@ -29,9 +29,9 @@ if (window.crypto) {
 
 window.addEventListener("message", messageListener);
 var template =
-    "echo url=" + window.location.protocol + "//" + username + ":$API_TOKEN@$API_HOST/ |" +
+    " echo url=" + window.location.protocol + "//" + username + ":$API_TOKEN@$API_HOST/ |" +
     " git -c credential.helper=store credential approve\n" +
-    "git clone -c credential.helper=store " + window.location.protocol + "//" + username + "@$API_HOST/ repo_" + username + "_RENAME_ME\n";
+    " git clone -c credential.helper=store " + window.location.protocol + "//" + username + "@$API_HOST/ repo_" + username + "_RENAME_ME\n";
 window.parent.postMessage({renderTemplate: {rpcId: "0", template: template}}, "*");
 
 });
