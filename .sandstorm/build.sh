514c1395976398ff271d5a6b6650b09fa50cd734
David Renshaw <dwrenshaw@gmail.com>
2015-08-17 16:19:45 -0400

    Cleanup.

--- .sandstorm/build.sh
+++ .sandstorm/build.sh
@@ -1,13 +1,2 @@
 #!/bin/bash
-# Checks if there's a composer.json, and if so, installs/runs composer.
 
-set -euo pipefail
-
-cd /opt/app
-
-if [ -f /opt/app/composer.json ] ; then
-    if [ ! -f composer.phar ] ; then
-        curl -sS https://getcomposer.org/installer | php
-    fi
-    php composer.phar install
-fi
